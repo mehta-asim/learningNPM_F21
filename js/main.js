0fdb12a253c24fe40f52b89576e13d323c743dd4
mehta-asim <72264877+mehta-asim@users.noreply.github.com>
2021-10-21 15:18:38 -0400

    php data connection succesfull

--- js/main.js
+++ js/main.js
@@ -4,34 +4,36 @@
   // set up a fetch fn to get some data
 
   function getData() {
-    fetch("./data.json") //go and get the data
+    fetch("./includes/functions.php") //go and get the data
       .then((res) => res.json()) //clean the data and convert it to a plain object
       .then((data) => {
         console.log(data);
 
-        buildTeam(data);
+        buildTeam(data[0]);
       })
       .catch((error) => console.error(error));
   }
 
   function buildTeam(info) {
+    debugger;
     //grab the keys from the data object (the names)
-    const people = Object.keys(info);
 
-    people.forEach((person) => {
+    info.forEach((person) => {
       let panel = theTemplate.cloneNode(true); // make a copy of template content
       let containers = panel.firstElementChild.children; //get a reference to the template content
+      debugger;
 
       // cycle through elements inside the <section> tage in <template> tag
       // and update their attributes
 
       //add the image
-      containers[0].querySelector("img").src = `images/${info[person].biopic}`;
+      containers[0].querySelector("img").src = `images/${person.biopic}`;
+      debugger;
 
       // update the text
-      containers[1].textContent = info[person].name;
-      containers[2].textContent = info[person].role;
-      containers[3].textContent = info[person].nickname;
+      containers[1].textContent = person.name;
+      containers[2].textContent = person.role;
+      containers[3].textContent = person.nickname;
 
       theTeam.appendChild(panel);
     });
