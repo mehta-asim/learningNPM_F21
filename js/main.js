c9229fa92b60f15f77e629045fb76798030c1b24
mehta-asim <72264877+mehta-asim@users.noreply.github.com>
2021-10-21 14:18:01 -0400

    adding static data

--- js/main.js
+++ js/main.js
@@ -1,44 +1,41 @@
 (() => {
-    const   theTeam = document.querySelector("#teamSection"),
-            theTemplate = document.querySelector("#bio-template").content;
+  const theTeam = document.querySelector("#teamSection"),
+    theTemplate = document.querySelector("#bio-template").content;
+  // set up a fetch fn to get some data
 
-    // set up a Fetch function and get some data
-    function getData() {
-        // retrieve our data object
-        fetch("./data.json") // go and get the data (fetch boy!)
-            .then(res => res.json()) // good dog! clean the stick (convert the data to a plain object)
-            .then(data => {
-                console.log(data);
+  function getData() {
+    fetch("./data.json") //go and get the data
+      .then((res) => res.json()) //clean the data and convert it to a plain object
+      .then((data) => {
+        console.log(data);
 
-                buildTeam(data);
-            })
-        .catch(error => console.error(error));
-    }
+        buildTeam(data);
+      })
+      .catch((error) => console.error(error));
+  }
 
-    function buildTeam(info) {
+  function buildTeam(info) {
+    //grab the keys from the data object (the names)
+    const people = Object.keys(info);
 
-        // grab the keys from the data object (the names)
-        const people = Object.keys(info);
+    people.forEach((person) => {
+      let panel = theTemplate.cloneNode(true); // make a copy of template content
+      let containers = panel.firstElementChild.children; //get a reference to the template content
 
-        people.forEach(person => {
-            let panel = theTemplate.cloneNode(true); // make a copy of the template content
-            let containers = panel.firstElementChild.children; // get a reference to the template content
+      // cycle through elements inside the <section> tage in <template> tag
+      // and update their attributes
 
-            // cycle through the child elements inside the <section> tag in the <template> tag
-            // and update their attributes 
-            
-            // add the image
-            containers[0].querySelector("img").src = `images/${info[person].biopic}`;
+      //add the image
+      containers[0].querySelector("img").src = `images/${info[person].biopic}`;
 
-            // update the text
-            containers[1].textContent = info[person].name;
-            containers[2].textContent = info[person].role;
-            containers[3].textContent = info[person].nickname;
+      // update the text
+      containers[1].textContent = info[person].name;
+      containers[2].textContent = info[person].role;
+      containers[3].textContent = info[person].nickname;
 
-            theTeam.appendChild(panel);
-        });
+      theTeam.appendChild(panel);
+    });
+  }
 
-    }
-
-    getData();
+  getData();
 })();
